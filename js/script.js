d184ec4019aff35189e20eacee1ef5484221513a
Carlos Eduardo Rocha Miranda <74724103+CarlosERM@users.noreply.github.com>
2021-10-19 13:44:44 -0300

    js fix

--- js/script.js
+++ js/script.js
@@ -1,31 +1,52 @@
-const inputinho = document.querySelector(".inputinho");
-const inputs = document.querySelectorAll("[placeholder]");
-
+const form = document.querySelector(".form");
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
-firstName.setCustomValidity("First name cannot be empty");
-lastName.setCustomValidity("Last name cannot be empty");
-email.setCustomValidity("Looks like this is not an email");
-password.setCustomValidity("Password cannot be empty");
 
-function handleForm(event) {
-  event.preventDefault();
-  console.log(inputs);
-  inputs.forEach((input) => {
-    if (!input.checkValidity()) {
-      console.log("oi");
-      input.previousElementSibling.classList.add("error");
-      input.nextElementSibling.classList.add("active");
+function handleForm(e) {
+  const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  e.preventDefault();
+  if(firstName.value == "") {
+    errorFunc(firstName, "First name cannot be empty");
+  } else {
+    sucessFunc(firstName);
+  }
 
-      input.nextElementSibling.innerHTML = input.validationMessage;
-    } else {
-      input.previousElementSibling.classList.remove("error");
-      input.nextElementSibling.classList.remove("active");
-    }
-  });
+  if(lastName.value == "") {
+    errorFunc(lastName, "Last name cannot be empty");
+  } else {
+    sucessFunc(lastName);
+  }
+
+
+  if(email.value == "") {
+    errorFunc(email, "Email cannot be empty");
+  } else if(!email.value.match(pattern)) {
+    errorFunc(email, "Looks like not an email");
+  } else {
+    sucessFunc(email);
+  }
+
+
+  if(password.value == "") {
+    errorFunc(password, "Password cannot be empty");
+  } else {
+    sucessFunc(password);
+  }
+
 }
 
-inputinho.addEventListener("click", handleForm);
+function errorFunc(element, message) {
+  element.previousElementSibling.classList.add("error");
+  element.nextElementSibling.classList.add("active");
+  element.nextElementSibling.innerHTML = message;
+}
+
+function sucessFunc(element) {
+  element.previousElementSibling.classList.remove("error");
+  element.nextElementSibling.classList.remove("active");
+}
+
+form.addEventListener("submit", handleForm);
